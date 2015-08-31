937efbde96b99458dd94c74d9e26766ed7c59018
tomuhedspi <tomuhedspi@gmail.com>
2015-08-31 14:15:06 +0700

    version 3

--- Model/Category.php
+++ Model/Category.php
@@ -4,7 +4,7 @@ class Category extends AppModel
     public function addCategory($data,$userId)
     {
         $this->create();
-        $this->user_id = $userId;
+        $data['user_id']= $userId;
        return  $this->save($data); 
     }
  
