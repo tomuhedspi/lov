4f097062641723b0c2396302714b1d5442b6b136
tomuhedspi <tomuhedspi@gmail.com>
2015-08-31 16:37:08 +0700

    change categories index file

--- Model/Category.php
+++ Model/Category.php
@@ -8,5 +8,19 @@ class Category extends AppModel
        return  $this->save($data); 
     }
  
+    public function getCategoriesByType($userId,$type)
+    {
+            $m_query = array
+            (
+               'condition' => array
+               (
+                  'Category.user_id' =>$userId,
+                  'Category.type'=>$type
+                )
+            );
+            $data= $this->find('all', $m_query);
+            //if not match userid and token
+            return $data;      
+    }
 }
 ?>
