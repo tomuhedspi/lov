8c8fd0ca63deec9e72e523e5f21a2661a83fe74d
tomuhedspi <tomuhedspi@gmail.com>
2015-09-01 14:59:11 +0700

    chinh sua lai index wallet

--- Model/Wallet.php
+++ Model/Wallet.php
@@ -1,8 +1,16 @@
-
 <?php
 class Wallet extends AppModel
 {
+    public function getWalletList($userId)
+    {
+        $data = $this->find('all', array( 'conditions' => array(  $this->alias . '.user_id' => $userId ) ));
+        return $data;
+    }
     
+    public function add($data, $id)
+    {
+           $this->create();
+           $data['user_id'] = $id;
+           return $this->save($data);
+    }
 }
-
-?>
