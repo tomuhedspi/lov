c884f63d385674f5dfa93ddc872eb8e00bde123c
tomuhedspi <tomuhedspi@gmail.com>
2015-09-03 14:32:40 +0700

    Anh Quang Check Cho User Xong Transfer Money Between Wallet

--- Model/User.php
+++ Model/User.php
@@ -1,187 +1,154 @@
 <?php
+
 App::uses('CakeEmail', 'Network/Email');
 App::uses('DigestAuthenticate', 'Controller/Component/Auth');
-class User extends AppModel
-    {
 
-    public  $validate =
-            array
-            (
-               'username' =>array
-                    (
-                       'notEmpty'=>array
-                               (
-                                 'rule'    => 'notBlank',
-                                 'message' => 'Please input user name !'
-                               ),
-                        'unique'=>array
-                                (
-                                  'rule'=>'isUnique',
-                                  'message'=>'Username has been used! Please try another name !'
-                                )
-                    ),
-                
-                'email'=>array
-                    (
-                       'notEmpty'=>array
-                               (
-                                 'rule'    => 'notBlank',
-                                 'message' => 'Please input user name !'
-                               ),
-                        'validEmail' => array
-                                (
-                                    'rule'    => array('email'),
-                                    'message' => 'Please enter a valid email dress'
-                                 ),
-                         'unique'=>array
-                                (
-                                  'rule'=>'isUnique',
-                                  'message'=>'This Email adress has been used! Try another email !'
-                                )
-                    ),
-                
-                'password' => array
-                    (
-                         'notEmpty' => array
-                                (
-                                    'rule'    => 'notBlank',
-                                    'message' => 'Please enter your password'
-                                )
-                    ),
-                
-                'confirm_password' => array
-                    (
-                           'notEmpty' => array
-                               (
-                                    'rule'    => 'notBlank',
-                                    'message' => 'Please Re_enter your password'
-                                ),
-                                'matchPass' => array
-                                (
-                                    'rule'    => 'passwordsMatch',
-                                    'message' => 'password do not match'
-                                )
-                    )
-            );
-  
-    
-  public function resetPassword($userId,$userToken,$inputData)  
-    {
-            $m_query = array
-            (
-               'condition' => array
-               (
-                  'User.id' =>$userId,
-                  'User.token'=>$userToken
-                )
-            );
-            $data= $this->find('first', $m_query);
-            //if not match userid and token
-            if (empty($data))
-            {
-                return FALSE;
-            }
-            //set activate true
-            $this->id = $userId;
-            $this->save
-                    ( array
-                        (
-                        'User'=> array
-                            (
-                            'token'=>NULL,
-                            'password'=>$inputData['password']
-                            )
-                        ) 
-                    );
-            return TRUE;
-    }
-    
-  public function  getUserEmailAndToken($username)
-  {
-    $m_query = array
-            (
-               'condition' => array
-               (
-                  'User.username' =>$username
-                )
-            );
-     $data= $this->find('first', $m_query);
-    //if cannot find userid
-    if (empty($data))
-    {   
-        return FALSE;
-    } 
-    //if found, set new token and return
-    $token= uniqid();
-    $result = $this->updateAll(
-        array('User.token' => "'$token'"),
-       // array('User.id' => $data['User']['id'])
-        array('User.username' => $username)    
+class User extends AppModel
+{
+
+    public $validate = array
+        (
+        'username'         => array(
+            'notEmpty' => array(
+                'rule'    => 'notBlank',
+                'message' => 'Please input user name !'
+            ),
+            'unique'   => array(
+                'rule'    => 'isUnique',
+                'message' => 'Username has been used! Please try another name !'
+            )
+        ),
+        'email'            => array(
+            'notEmpty'   => array(
+                'rule'    => 'notBlank',
+                'message' => 'Please input user name !'
+            ),
+            'validEmail' => array(
+                'rule'    => array('email'),
+                'message' => 'Please enter a valid email adress'
+            ),
+            'unique'     => array(
+                'rule'    => 'isUnique',
+                'message' => 'This Email adress has been used! Try another email !'
+            )
+        ),
+        'password'         => array(
+            'notEmpty' => array(
+                'rule'    => 'notBlank',
+                'message' => 'Please enter your password'
+            ),
+            'length'   => array(
+                'rule'    => array('between', 5, 256),
+                'message' => 'The password must be between 5 and 256 characters.'
+            ),
+        ),
+        'confirm_password' => array(
+            'notEmpty'  => array(
+                'rule'    => 'notBlank',
+                'message' => 'Please Re_enter your password'
+            ),
+            'matchPass' => array(
+                'rule'    => 'passwordsMatch',
+                'message' => 'password do not match'
+            )
+        )
     );
-    //check if update token success?
-    if (!$result)
+
+    public function resetPassword($userId, $userToken, $inputData)
     {
-        return FALSE;
+        $m_query = array(
+            'condition' => array(
+                'User.id'    => $userId,
+                'User.token' => $userToken
+            )
+        );
+        $data    = $this->find('first', $m_query);
+        //if not match userid and token
+        if (empty($data)) {
+            return false;
+        }
+        //set activate true
+        $this->id = $userId;
+        $result   = $this->save(array(
+            'User' => array(
+                'token'    => NULL,
+                'password' => $inputData['password']
+        )));
+        return $result;
     }
-    $data= $this->find('first', $m_query);
-    return $data;
-  }
-  
-  public function beforeSave($options = array())
+
+    public function getUserEmailAndToken($username)
+    {
+        $m_query = array(
+            'condition' => array(
+                'User.username' => $username
+        ));
+        $data    = $this->find('first', $m_query);
+        //if cannot find userid
+        if (empty($data)) {
+            return false;
+        }
+        //if found, set new token and return
+        $token  = uniqid();
+        $result = $this->updateAll(
+                array('User.token' => "'$token'"),
+                // array('User.id' => $data['User']['id'])
+                array('User.username' => $username)
+        );
+        //check if update token success?
+        if (!$result) {
+            return false;
+        }
+        $data = $this->find('first', $m_query);
+        return $data;
+    }
+
+    public function beforeSave($options = array())
     {
         if (isset($this->data['User']['password'])) {
             $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
         }
         return true;
     }
- 
-    public function activate($userId,$userToken)
-        {
-            $m_query = array
-            (
-               'condition' => array
-               (
-                  'User.id' =>$userId,
-                  'User.token'=>$userToken
-                )
-            );
-            $data= $this->find('first', $m_query);
-            //if not match userid and token
-            if (empty($data))
-            {
-                return FALSE;
-            }
-            //set activate true
-            $this->id = $userId;
-            $this->save
-                    ( array
-                        (
-                        'User'=> array
-                            (
-                            'token'=>NULL,
-                            'activated'=>true
-                            )
-                        ) 
-                    );
-            return TRUE;
-        }
 
-    public function passwordsMatch($data)
-        {
-            if ($this->data['User']['password'] == $this->data['User']['confirm_password']) {
-                return true;
-            }
+    public function activate($userId, $userToken)
+    {
+        $m_query = array(
+            'condition' => array(
+                'User.id'    => $userId,
+                'User.token' => $userToken
+        ));
+        $data    = $this->find('first', $m_query);
+        //if not match userid and token
+        if (empty($data)) {
             return false;
         }
-    
-    public function  createUser($data)
-        {
-            $this->create();
-            $data['token']=  uniqid();
-            $data['activated']= FALSE;
-            return $this->save($data);
-        }
-    
+        //set activate true
+        $this->id = $userId;
+        $this->save(array(
+            'User' => array(
+                'token'     => NULL,
+                'activated' => true
+        )));
+        return true;
     }
+
+    public function passwordsMatch($data)
+    {
+        if ($this->data['User']['password'] == $this->data['User']['confirm_password']) {
+            return true;
+        }
+        return false;
+    }
+
+    public function createUser($data)
+    {
+        $this->create();
+        $data['token']     = uniqid();
+        $data['activated'] = false;
+        return $this->save($data);
+    }
+
+}
 
 ?>
--- Model/Wallet.php
+++ Model/Wallet.php
@@ -1,13 +1,61 @@
 <?php
 class Wallet extends AppModel
 {
+    public function transfer($fromId,$toId,$amount)
+    {
+        //start transaction
+        $dataSource = $this->getDataSource();
+        $dataSource->begin();
+        //get money from wallet 1   
+        $data = $this->findById($fromId);
+        $data['Wallet']['amount']-= $amount;
+        $result1= $this->save($data);
+        //put money to wallet 2
+        $data = $this->findById($toId);
+        $data['Wallet']['amount']+= $amount;
+        $result2= $this->save($data);        
+        //finish transaction
+        if($result1&&$result2){
+            return $dataSource->commit();
+        }  else {
+            $dataSource->rollback();
+            return  false;
+        }
+    }
+    
+    public function moneyInWallet($walletId)
+    {
+       $this->id=$walletId;
+       $data = $this->findById($walletId);
+       return $data['Wallet']['amount'];
+    }
+
+    public function walletBelongUser($userId,$walletId)
+    {
+        $data = $this->find('first',
+           array(
+               'conditions' => array( 'Wallet.id'=>$walletId,'Wallet.user_id' => $userId)
+                 ));
+        return $data;   
+    }
+    
+    public  function getWalletNameID($userId)//danh sach wallet su dung cho transfer
+    {
+        $data = $this->find('list',
+                array(
+                    'conditions' => array('Wallet.user_id' => $userId),
+                    'fields' => array('Wallet.id', 'Wallet.name')
+                      ));
+        return $data;
+    }
+    
     public function edit($data,$id)
     {
         $this->id = $id;
         return $this->save($data);
     }
     
-    public function getWalletList($userId)
+    public function getWalletList($userId)//all data of wallet
     {
         $data = $this->find('all', array( 'conditions' => array(  $this->alias . '.user_id' => $userId ) ));
         return $data;
